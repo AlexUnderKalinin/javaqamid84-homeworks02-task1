ea0ed97b821f15ec0107208c65b8e159aa946346
Alexander Kalinin <legalcounsel42@gmail.com>
2024-03-25 22:32:23 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int x;
+        x = 20;
+        int bonusMiles = price / x;
+
+        System.out.println("Количество бонусных миль " + bonusMiles);
+    }
+}
